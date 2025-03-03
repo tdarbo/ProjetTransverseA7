976b9d18e89bcc67bf108d0b2124f39675466540
GardeDeNuit <matteo.spindler17@gmail.com>
2025-03-03 17:40:19 +0100

    Add scene management logic and main scene

--- code/game.py
+++ code/game.py
@@ -3,43 +3,44 @@ from scene import *
 
 class Game:
     def __init__(self):
+        # Initialize Pygame
         pygame.init()
         pygame.display.set_caption(GAME_NAME)
 
+        # Set up the game window
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.running = True
         self.clock = pygame.time.Clock()
         self.time_delta = DEFAULT_TIME_DELTA
-        # Interfaces avec pygame_ui
-        self.ui_manager = pygame_gui.UIManager((WINDOW_WIDTH, WINDOW_HEIGHT), theme_path="../data/ui-theme.json")
-        self.ui_manager.set_visual_debug_mode(UI_DEBUG_MODE)
 
-        SPLASH_SCREEN_SCENE = SplashScreenScene()
-        MAIN_MENU_SCENE = MainMenuScene(self.ui_manager)
-
-        # Gestion des scenes du jeu
+        # Initialize the scene manager
         self.scene_manager = SceneManager()
-        self.scene_manager.add("splash", SPLASH_SCREEN_SCENE)
-        self.scene_manager.add("main", MAIN_MENU_SCENE)
-        self.scene_manager.change("main")
+
+        # Create and add the scenes
+        main_menu_scene = MainMenuScene(self)
+        self.scene_manager.add("main_menu_scene", main_menu_scene)
+        self.scene_manager.change("main_menu_scene")
 
     def run(self):
+        # Main game loop
         while self.running:
+            # Calculate time delta
             self.time_delta = self.clock.tick(FPS) / 1000
 
+            # Event handling
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
                 self.scene_manager.process_event(event)
-                self.ui_manager.process_events(event)
 
-            self.screen.fill(WHITE)
-
+            # Update and draw the current scene
             self.scene_manager.update(self.time_delta)
             self.scene_manager.draw(self.screen)
-            self.ui_manager.update(self.time_delta)
+
+            # Update the display
             pygame.display.flip()
 
 if __name__ == "__main__":
+    # Create a game instance and run it
     game = Game()
     game.run()
