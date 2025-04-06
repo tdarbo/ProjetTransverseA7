8cfc4d5f4b85094dca025ed2d85f5393dbf42207
GardeDeNuit <matteo.spindler17@gmail.com>
2025-04-06 22:37:27 +0200

    Fonction d'affichage du score

--- code/score.py
+++ code/score.py
@@ -1,6 +1,8 @@
 from settings import *
 from ui_text import Text
 
+WIDTH = 1200
+HEIGHT = 500
 class ScoreManager:
     """
     This class manages the scores of all the players during the game.
@@ -9,32 +11,25 @@ class ScoreManager:
     """
 
     def __init__(self, players: list, hole_number: int):
-        self.score = {}  # Dictionary that contains the scores of the players
         self.players = players
         self.hole_number = hole_number
+        self.score = self.create_dictionary()  # Dictionary that contains the scores of the players
 
     def create_dictionary(self):
-        """
-        Creates a dictionary using the number of players and the number of hole,
-        In order to associate the scores of the games to the players
-        """
+        score_dict = dict()
         for player in self.players:
-            if player not in self.score:
-                self.score[player] = dict()
+            if player not in score_dict:
+                score_dict[player] = dict()
             player_score = [0] * self.hole_number
-            self.score[player]["score"] = player_score
-            self.score[player]["total"] = 0
+            score_dict[player]["score"] = player_score
+            score_dict[player]["total"] = 0
+
+        return score_dict
 
     def add_points(self, player, hole):
-        """
-        Increase the score of the player at the end of his turn
-        """
         self.score[player]["score"][hole] += 1
 
     def score_calculation(self):
-        """
-        Calculates the global score for each player
-        """
         for player in self.players:
             total = 0
             for i in range(self.hole_number):
@@ -42,9 +37,6 @@ class ScoreManager:
             self.score[player]["total"] = total
 
     def score_reset(self):
-        """
-        Set the score of every player at 0 for every turn
-        """
         for player in self.players:
             tab = [0] * self.hole_number  # Adding a line to remove the total
             self.score[player]["score"] = tab
@@ -54,7 +46,7 @@ class ScoreManager:
         line = []
         line.append("Trou n°")
         for value in self.score.keys(): # Keys = Noms des joueurs
-            line.append(value)
+            line.append(value.name)
 
         return line
 
@@ -74,16 +66,53 @@ class ScoreManager:
 
         return line
 
-    def draw(self):
-        self.print_score()
-        Text("Cliquez \npour changer", (100, 150), color="BLACK")
+    def draw(self, screen):
+        # On construit un tableau à partir des données du tableau
+        header = self._get_table_header()
+        footer = self._get_table_footer()
+        lines = [header]
+        for i in range(self.hole_number):
+            lines.append(self._get_table_line(i))
+        lines.append(footer)
+
+        # Dimension des cellules
+        cell_width = 100 # Largeur d'une cellule
+        cell_height = 10 # Hauteur d'une cellule
+        cell_gap = 5 # Espace entre cellules
+
+        # Dimension du menu
+        menu_gap = 50
+        menu_height = (len(lines) - 1) * cell_gap + cell_height * len(lines)
+        menu_width = (len(lines[0]) - 1) * cell_gap + cell_width * len(lines[0])
+
+        # Calcul de la position de la première cellule pour que notre menu soit positionné à partir du coin inférieur droit
+        start_x = screen.get_width() - menu_gap - menu_width
+        start_y = screen.get_height() - menu_gap - menu_height
+
+        # On dessine un rectangle englobant le menu
+        pygame.draw.rect(screen, (255, 255, 255), (start_x, start_y, menu_width, menu_height), 3)
+
+        # On affiche le tableau du score ligne par ligne
+        for row_index in range(len(lines)):
+            row = lines[row_index]
+            for col_index in range(len(lines[0])):
+                cell = row[col_index]
+                current_cell_x = start_x + col_index * (cell_width + cell_gap)
+                current_cell_y = start_y + row_index * (cell_height + cell_gap)
+
+                text_obj = Text(
+                    text=str(cell),
+                    pos=(current_cell_x, current_cell_y),
+                    color=(255, 255, 255),
+                    font_size=10
+                )
+                text_obj.draw(screen)  # On affiche le texte de la cellule
 
     def print_score(self):
         print(self._get_table_header())
         for i in range(self.hole_number):
             print(self._get_table_line(i))
         print(self._get_table_footer())
-
 
 if __name__ == '__main__':
     players = ["Louis", "Mathias", "Bastien", "Mattéo", "Thomas"]
@@ -102,3 +131,7 @@ if __name__ == '__main__':
     # score_manager.score_reset()
     print(score_manager.score, "après reset")
     score_manager.print_score()
+    #score_manager.draw()
+
+    for i in range(4, -1, -1):
+        print(i)
