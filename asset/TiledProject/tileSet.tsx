79dc118ff39a985a7824563e2150f30cb17db897
mathas42 <mathias.leroy@efrei.net>
2025-05-02 11:31:13 +0200

    règler problème

--- asset/TiledProject/tileSet.tsx
+++ asset/TiledProject/tileSet.tsx
@@ -1,11 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileSet" tilewidth="64" tileheight="64" tilecount="9" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="tileSet" tilewidth="64" tileheight="64" tilecount="17" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../image/tiles/dirt.png" width="64" height="64"/>
- </tile>
- <tile id="18">
-  <image source="../image/tiles/water-dirt.png" width="64" height="64"/>
  </tile>
  <tile id="1">
   <image source="../image/tiles/grass.png" width="64" height="64"/>
@@ -16,9 +13,6 @@
  <tile id="6">
   <image source="../image/tiles/ice.png" width="64" height="64"/>
  </tile>
- <tile id="14">
-  <image source="../image/tiles/water.png" width="64" height="64"/>
- </tile>
  <tile id="3">
   <image source="../image/tiles/border.png" width="64" height="64"/>
   <objectgroup draworder="index" id="2">
@@ -28,34 +22,40 @@
    <object id="4" x="0" y="0"/>
   </objectgroup>
  </tile>
- <tile id="24">
-  <image source="../image/tiles/box.png" width="64" height="64"/>
- </tile>
  <tile id="13">
   <image source="../image/tiles/lava-dirt.png" width="64" height="64"/>
  </tile>
- <tile id="23">
-  <image source="../image/tiles/Orientation=bottom.png" width="64" height="64"/>
+ <tile id="14">
+  <image source="../image/tiles/water.png" width="64" height="64"/>
  </tile>
- <tile id="22">
-  <image source="../image/tiles/Orientation=corner-bottom-left.png" width="64" height="64"/>
- </tile>
- <tile id="21">
-  <image source="../image/tiles/Orientation=corner-bottom-right.png" width="64" height="64"/>
- </tile>
- <tile id="20">
-  <image source="../image/tiles/Orientation=corner-top-left.png" width="64" height="64"/>
- </tile>
- <tile id="19">
-  <image source="../image/tiles/Orientation=corner-top-right.png" width="64" height="64"/>
- </tile>
- <tile id="17">
-  <image source="../image/tiles/Orientation=left.png" width="64" height="64"/>
+ <tile id="15">
+  <image source="../image/tiles/Orientation=top.png" width="64" height="64"/>
  </tile>
  <tile id="16">
   <image source="../image/tiles/Orientation=right.png" width="64" height="64"/>
  </tile>
- <tile id="15">
-  <image source="../image/tiles/Orientation=top.png" width="64" height="64"/>
+ <tile id="17">
+  <image source="../image/tiles/Orientation=left.png" width="64" height="64"/>
+ </tile>
+ <tile id="18">
+  <image source="../image/tiles/water-dirt.png" width="64" height="64"/>
+ </tile>
+ <tile id="19">
+  <image source="../image/tiles/Orientation=corner-top-right.png" width="64" height="64"/>
+ </tile>
+ <tile id="20">
+  <image source="../image/tiles/Orientation=corner-top-left.png" width="64" height="64"/>
+ </tile>
+ <tile id="21">
+  <image source="../image/tiles/Orientation=corner-bottom-right.png" width="64" height="64"/>
+ </tile>
+ <tile id="22">
+  <image source="../image/tiles/Orientation=corner-bottom-left.png" width="64" height="64"/>
+ </tile>
+ <tile id="23">
+  <image source="../image/tiles/Orientation=bottom.png" width="64" height="64"/>
+ </tile>
+ <tile id="24">
+  <image source="../image/tiles/box.png" width="64" height="64"/>
  </tile>
 </tileset>
