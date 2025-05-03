9800ad626441507d8fea39e4a7c7ac4fdcf5a3c6
matte <matteo.spindler17@gmail.com>
2025-05-04 00:39:04 +0200

    Nouvelles variables pour le tableau des scores et réorganisations
    Signed-off-by: matte <matteo.spindler17@gmail.com>

--- code/settings.py
+++ code/settings.py
@@ -2,62 +2,68 @@ import pygame
 import pygame_gui
 from pygame.math import Vector2 as Vector
 
+## Chemins des fichiers du jeu
+ASSET_PATH = "../asset"
+UI_THEME_PATH = "../data/ui-theme.json"
+FONT_PATH = "../asset/font/font-regular-v2.ttf"
+
+## Paramètres de configuration du jeu
 GAME_NAME = "GOAT"
 GAME_VERSION = "1.0"
-
+DEBUG_MODE = False
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 FPS = 60
-DELTA_TIME = 0
+TILE_SIZE = 64
+BALL_RADIUS = 15
 
-# Physic
+## Paramètres de configuration d'une partie
+MAX_PLAYERS_NUMBER = 5
+MAX_HOLES_NUMBER = 5
+MAX_PLAYER_NAME_LEN = 12
+DEFAULT_GAME_DICTIONARY = {"players": 1, "holes": 1, "names": []}
+PLAYER_COLORS = [(255, 0, 0), (0, 0, 255), (0, 255, 0), (255, 255, 0), (0, 255, 255), (255, 0, 255)]
+
+## Variables physiques
+DELTA_TIME = 0
 VELOCITY_THRESHOLD = 10
 MAX_PLAYER_VELOCITY = Vector(1000, 1000)
 FORCE_MULTIPLIER = 5
-BALL_RADIUS = 15
-TILE_SIZE = 64
 BALL_MASS = 0.05
 GROUND_GRASS_FRICTION = 0.06
 GROUND_SAND_FRICTION = 0.30
 GROUND_ICE_FRICTION = 0.02
 
+## Paramètres des composants d'interfaces
 INPUT_WIDTH = 300
 INPUT_HEIGHT = 52
 INPUTS_GAP = 10
 PANEL_WIDTH = 500
 PANEL_HEIGHT = WINDOW_HEIGHT + 10
-PANEL_LAYOUT = pygame.Rect(
-    (WINDOW_WIDTH - PANEL_WIDTH) // 2,
-    -5,
-    PANEL_WIDTH,
-    PANEL_HEIGHT
-)
+PANEL_LAYOUT = pygame.Rect((WINDOW_WIDTH - PANEL_WIDTH) // 2, -5, PANEL_WIDTH, PANEL_HEIGHT)
 PANEL_MARGINS = {
     "top": 100,
+    "bottom": 50,
     "left": 50,
-    "right": 50,
-    "bottom": 50
+    "right": 50
 }
-
-DEFAULT_GAME_DICTIONARY = {"players": 1, "holes": 1, "names": []}
-PLAYER_COLORS = [(255, 0, 0), (0, 0, 255), (0, 255, 0), (255, 255, 0), (0, 255, 255), (255, 0, 255)]
-
 SCENE_BG_IMAGE = loadingScreen = pygame.transform.scale(
     pygame.image.load('../asset/image/terrain_bg.jpg'),
     (WINDOW_WIDTH, WINDOW_HEIGHT)
 )
 
-DEBUG_MODE = False
-
-ASSET_PATH = "../asset"
-UI_THEME_PATH = "../data/ui-theme.json"
-FONT_PATH = "../asset/font/font-regular-v2.ttf"
-
+## Paramètres de la caméra
 MAX_ZOOM = 4
 MIN_ZOOM = 0.5
-MAX_PLAYERS_NUMBER = 5
-MAX_HOLES_NUMBER = 5
-MAX_PLAYER_NAME_LEN = 12
 
+## Dimensions de la fenêtre d'erreur
 WINDOW_ERROR_WIDTH = PANEL_WIDTH - 100
 WINDOW_ERROR_HEIGHT = PANEL_HEIGHT // 2
+
+## Dimensions du menu du score
+SCORE_CELL_WIDTH = 100 # Largeur d'une cellule
+SCORE_CELL_HEIGHT = 10 # Hauteur d'une cellule
+SCORE_CELL_GAP = 15 # Espace entre cellules
+SCORE_MENU_PADDING = 20 # Espace entre le contenu et le menu
+SCORE_MENU_MARGIN = 25 # Espace entre le menu et les bords de l'écran
+SCORE_MENU_FONT_SIZE = 10 # Taille de la police du menu
