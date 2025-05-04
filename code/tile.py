f5e0e5d2e8332a59461bad28f53e99b9c1ce9da3
Louis <l38.lemonnier@gmail.com>
2025-05-04 13:50:44 +0200

    Simplification de la class, nouvelles méthodes pour obtenir les coordonnées et la taille, et commentaires
    Signed-off-by: matte <matteo.spindler17@gmail.com>

--- code/tile.py
+++ code/tile.py
@@ -1,44 +1,39 @@
-
-from camera import Camera
 from settings import *
-import pygame, pytmx
+
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_id, x, y, width, height, color=None, image_path=None, image_surface=None):
+    def __init__(self, tile_type_id, x, y, width, height, image_surface):
         super().__init__()
-        self.id = tile_id
 
-        if image_surface:
-            self.image = pygame.transform.scale(image_surface, (width, height))
-        elif image_path:
-            try:
-                self.image = pygame.image.load(image_path).convert_alpha()
-                self.image = pygame.transform.scale(self.image, (width, height))
-            except pygame.error as e:
-                raise ValueError(f"Erreur lors du chargement de l'image: {image_path}. Détail: {e}")
-        elif color:
-            self.image = pygame.Surface((width, height))
-            if isinstance(color, str):
-                color = pygame.Color(color)
-            self.image.fill(color)
-        else:
-            raise ValueError("Vous devez fournir une couleur, un chemin d'image ou une surface Pygame pour créer une Tile.")
+        # Identifiant du type de la tuile
+        # Ex : "Sand", "Grass", "Water", ...
+        self.id = tile_type_id
 
-        self.x, self.y = x, y
+        # Surface de l'image associée à la tuile
+        self.image = image_surface
+
+        # On initialise la position de la tuile
+        # get_rect() est utilisée pour obtenir un rectangle représentant la position et la taille de l'image
+        # Le rectangle est positionné en haut à gauche aux coordonnées (x, y)
         self.rect = self.image.get_rect(topleft=(x, y))
 
-    def is_on_screen(self, camera:Camera) -> bool:
-
-        t_l, t_r, b_l, b_r = (self.x,self.y),(self.x+TILE_SIZE,self.y),(self.x,self.y+TILE_SIZE),(self.x+TILE_SIZE,self.y+TILE_SIZE)
-        on_screen:bool = True
-
-        if not camera.is_world_position_on_screen(t_l[0],t_l[1]) and not camera.is_world_position_on_screen(t_r[0],t_r[1]) and not camera.is_world_position_on_screen(b_l[0],b_l[1]) and not camera.is_world_position_on_screen(b_r[0],b_r[1]):
-            on_screen = False
-
-        return on_screen
-
     def draw(self, surface):
+        # Permet de dessiner la tuile sur une surface donnée
+        # blit() est utilisée pour copier le contenu de l'image sur la surface aux coordonnées spécifiées par le rectangle
         surface.blit(self.image, self.rect)
 
-    def show(self, surface):
-        surface.blit(self.image, self.rect)
+    @property
+    def x(self):
+        return self.rect.x
+
+    @property
+    def y(self):
+        return self.rect.y
+
+    @property
+    def width(self):
+        return self.rect.width
+
+    @property
+    def height(self):
+        return self.rect.height
