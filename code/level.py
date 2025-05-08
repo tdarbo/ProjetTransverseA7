b10d27bfa03c38c7603485e5c188f75c8c84efa7
matte <matteo.spindler17@gmail.com>
2025-05-08 19:08:51 +0200

    Ajout des types des variables, renommage et petites corrections
    Signed-off-by: matte <matteo.spindler17@gmail.com>

--- code/level.py
+++ code/level.py
@@ -1,76 +1,72 @@
-from time import sleep
-
-import pygame
-
 from bonus_manager import BonusSpeed, BonusType
 from settings import *
 from engine import Engine
+from score import ScoreManager
 from broadcast import BroadcastManager
+from player import Player
+from map import Map
 
 
 class Level:
     """Gestion de la map, des tours et des événements"""
 
-    def __init__(self, hole_number, tiled_map, players, score_manager, broadcast_manager, game,screen_width=1280,
-                 screen_height=720):
+    def __init__(self, hole_index, tiled_map, players, score_manager, broadcast_manager, game):
         """Initialise le niveau."""
-        self.map = tiled_map
-        self.players = players
-        self.engine = Engine(self)
-        self.score_manager = score_manager
-        self.broadcast_manager = broadcast_manager
-        self.current_player_index = 0
-        self.current_player = players[0]
-        self.shot_taken = False  # Indique si le joueur actif a joué
-        self.hole_number = hole_number  # Numéro du trou associé au level
+        self.game = game
+        self.map: Map = tiled_map
+        self.players: list[Player] = players
+        self.engine: Engine = Engine(self)
+        self.score_manager: ScoreManager = score_manager
+        self.broadcast_manager: BroadcastManager = broadcast_manager
+        self.cur_player_index: int = 0
+        self.cur_player: Player = players[0]
+        self.shot_taken: bool = False  # Indique si le joueur actif a joué
+        self.hole_index = hole_index  # Numéro du trou associé au level
 
         # Variables pour le drag & drop
-        self.dragging = False
-        self.drag_start = None  # Vector
-        self.drag_current = None  # Vector
+        self.dragging: bool = False
+        self.drag_start: Vector = None
+        self.drag_current: Vector = None
 
-        self.force_multiplier = FORCE_MULTIPLIER
-
-        self.finished = False
+        self.finished: bool = False
 
         # Surfaces d'affichage
         self.map_size = (self.map.map_width, self.map.map_height)
-        self.overlay_surf = pygame.Surface((screen_width, screen_height)).convert_alpha()
-        self.map_surf = pygame.Surface(self.map_size).convert()
+        self.overlay_surf: pygame.surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT)).convert_alpha()
+        self.map_surf: pygame.surface = pygame.Surface(self.map_size).convert()
 
         # self.gif_manager.add_gif("../asset/GIF/Cactus.gif", 1511, 153, .5, True, False)
-        self.bonus_gifs = []
+        self.bonus_gifs: list[str] = []
 
         self.map.teleportPlayersToSpawn(self.players)
-        self.centerOnCurrentPlayer()
-        self.game = game
+        self.centerOnPlayer(self.cur_player)
 
     def process_event(self, event):
         """Evénements pygame."""
         self.map.camera.process_event(event)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                self.centerOnCurrentPlayer()
+                self.centerOnPlayer(self.cur_player)
             elif event.key == pygame.K_h:
-                self.current_player.position.x = self.map.hole.x
-                self.current_player.position.y = self.map.hole.y
+                self.cur_player.position.x = self.map.hole.x
+                self.cur_player.position.y = self.map.hole.y
             elif event.key == pygame.K_e:
-                if isinstance(self.current_player.bonus, BonusType):
-                    self.current_player.bonus.consume_bonus(self.current_player, self.players)
+                if isinstance(self.cur_player.bonus, BonusType):
+                    self.cur_player.bonus.consume_bonus(self.cur_player, self.players)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             self.on_mouse_down(event)
         elif event.type == pygame.MOUSEMOTION:
             self.on_mouse_motion(event)
         elif event.type == pygame.MOUSEBUTTONUP:
-            self.on_mouse_up(event)
+            self.handle_shot(event)
 
     def on_mouse_down(self, event):
         if self.shot_taken:
             return
         adjusted_pos = self.map.camera.getAbsoluteCoord(event.pos)
-        if self.current_player.rect.collidepoint(adjusted_pos):
+        if self.cur_player.rect.collidepoint(adjusted_pos):
             self.dragging = True
-            self.drag_start = Vector(self.current_player.rect.center)
+            self.drag_start = Vector(self.cur_player.rect.center)
             self.drag_current = Vector(adjusted_pos)
 
     def on_mouse_motion(self, event):
@@ -78,33 +74,22 @@ class Level:
             adjusted_pos = self.map.camera.getAbsoluteCoord(event.pos)
             self.drag_current = Vector(adjusted_pos)
 
-    def on_mouse_up(self, event):
+    def handle_shot(self, event):
         if self.dragging:
             self.game.sound_manager.play_sound(SOUNDS["ball"])
-            # On modifie le score du joueur
-            # Il vient de jouer donc on lui ajoute 1 point
-            self.score_manager.add_points(self.current_player, self.hole_number)
-
-            if isinstance(self.current_player.bonus, BonusSpeed):
-                self.current_player.bonus.consume_bonus(self.current_player, self.players)
+            self.score_manager.add_points(self.cur_player, self.hole_index)
 
             adjusted_pos = self.map.camera.getAbsoluteCoord(event.pos)
-            new_velocity = (self.drag_start - adjusted_pos) * self.force_multiplier
-            # if self.current_player.speed_bonus:
-            #    new_velocity *= 2
-            #    new_velocity = min(new_velocity, MAX_PLAYER_VELOCITY.length()*1.5)
-            if new_velocity.length() >= MAX_PLAYER_VELOCITY.length():
-                new_velocity = new_velocity.normalize() * MAX_PLAYER_VELOCITY.length()
+            velocity_vector = (self.drag_start - adjusted_pos) * FORCE_MULTIPLIER
+            self.engine.resolve_shot(self.cur_player, velocity_vector)
 
-            self.current_player.velocity += new_velocity
             self.dragging = False
             self.shot_taken = True
             self.drag_start = None
             self.drag_current = None
 
-
     def update(self, dt):
-        if self.current_player.finished:
+        if self.cur_player.finished:
             self.shot_taken = True
 
         self.update_bonuses()
@@ -114,59 +99,58 @@ class Level:
 
     def update_gifs(self):
         self.map.load_gif_bonuses(self.map_surf)
-        self.current_player.update_gifs(self.overlay_surf)
+        self.cur_player.update_gifs(self.overlay_surf)
 
     def update_bonuses(self):
         for bonus in self.map.bonuses:
             bonus.update_bonus(self.map_surf)
-        if isinstance(self.current_player.bonus, BonusType):
-            self.bonus_gifs.append(self.current_player.bonus.icon_id)
+        if isinstance(self.cur_player.bonus, BonusType):
+            self.bonus_gifs.append(self.cur_player.bonus.icon_id)
         else:
-            if DEBUG_MODE: self.current_player.bonus = BonusSpeed()
+            if DEBUG_MODE: self.cur_player.bonus = BonusSpeed()
 
     def check_turn_end(self):
         """Vérifie si le tour est terminé et passe au joueur suivant."""
         if self.shot_taken:
-            self.centerOnPlayers()
-            if self.current_player.velocity.length() < VELOCITY_THRESHOLD:
+            self.centerOnPlayers(self.players)
+            if self.cur_player.velocity.length() < VELOCITY_THRESHOLD:
                 self.next_turn()
 
     def next_turn(self):
         """Passe au tour du joueur suivant."""
         self.shot_taken = False
         for i in range(len(self.players)):
-            self.current_player_index = (self.current_player_index + 1) % len(self.players)
-            self.current_player = self.players[self.current_player_index]
-            if not self.current_player.finished:
-                self.broadcast_manager.broadcast(f"Tour du joueur {self.current_player_index + 1}")
-                print(f"Tour du joueur {self.current_player_index + 1}")
-                self.centerOnCurrentPlayer()
+            self.cur_player_index = (self.cur_player_index + 1) % len(self.players)
+            self.cur_player = self.players[self.cur_player_index]
+            if not self.cur_player.finished:
+                self.broadcast_manager.broadcast(f"Tour du joueur {self.cur_player_index + 1}")
+                print(f"Tour du joueur {self.cur_player_index + 1}")
+                self.centerOnPlayer(self.cur_player)
                 return
             print("skipped turn")
         # Aucun joueur actif
         self.finished = True
 
-    def get_line_color(self, line_length: int) -> str:
-        global width_line
-        if line_length < 100:
+    def get_line_parameters(self, length: int) -> (str, int):
+        width = 6
+        color = "red"
+        if length < 100:
             color = "pink"
-            width_line = 1
-        elif line_length < 200:
+            width = 1
+        elif length < 200:
             color = "blue"
-            width_line = 2
-        elif line_length < 300:
+            width = 2
+        elif length < 300:
             color = "green"
-            width_line = 3
-        elif line_length < 400:
+            width = 3
+        elif length < 400:
             color = "yellow"
-            width_line = 4
-        elif line_length < 500:
+            width = 4
+        elif length < 500:
             color = "orange"
-            width_line = 5
-        else:
-            color = "red"
-            width_line = 6
-        return color
+            width = 5
+
+        return color, width
 
     def world_to_screen_position(self, world_pos, center_point, camera_zoom):
         return (
@@ -202,24 +186,25 @@ class Level:
                 player.draw(self.map_surf)
 
         # Cerclage du joueur actif s'il n'a pas encore joué
-        if not self.shot_taken and not self.current_player.finished:
+        if not self.shot_taken and not self.cur_player.finished:
             pygame.draw.circle(
                 surface=self.map_surf,
                 color=pygame.Color("white"),
-                center=self.current_player.rect.center,
-                radius=self.current_player.radius + 5,
+                center=self.cur_player.rect.center,
+                radius=self.cur_player.radius + 5,
                 width=2
             )
 
         # Dessin de la ligne de visé
         if self.dragging and self.drag_current is not None:
-            line_length = (self.current_player.position - self.drag_current).length()
+            line_length = int((self.drag_start - self.drag_current).length())
+            line_color, line_width = self.get_line_parameters(line_length)
             pygame.draw.line(
-                self.overlay_surf,
-                pygame.Color(self.get_line_color(line_length)),
-                self.world_to_screen_position(self.current_player.position, center, zoom),
-                self.world_to_screen_position(self.drag_current, center, zoom),
-                width=width_line
+                surface=self.overlay_surf,
+                color=line_color,
+                start_pos=self.world_to_screen_position(self.cur_player.position, center, zoom),
+                end_pos=self.world_to_screen_position(self.drag_current, center, zoom),
+                width=line_width
             )
 
         resize_size = (int(self.map_size[0] * zoom), int(self.map_size[1] * zoom))
@@ -239,25 +224,24 @@ class Level:
         self.score_manager.draw(self.overlay_surf)
         self.broadcast_manager.draw(self.overlay_surf)
         screen.blit(self.overlay_surf, (0, 0))
-        # print(self.map.camera.is_world_position_on_screen(self.current_player.position.x, self.current_player.position.y))
+        # print(self.map.camera.is_world_position_on_screen(self.cur_player.position.x, self.cur_player.position.y))
 
-    def centerOnCurrentPlayer(self):
-        player = self.current_player
+    def centerOnPlayer(self, player: Player):
         x = player.position.x
         y = player.position.y
 
         camera = self.map.camera
         camera.animator.posToPosAndZoom(camera, (x, y), 1.0, 120)
 
-    def centerOnPlayers(self):
+    def centerOnPlayers(self, players: list[Player]):
         camera = self.map.camera
         x, y = 0, 0
-        for player in self.players:
+        for player in players:
             x += player.position.x
             y += player.position.y
 
-        p_count = len(self.players)
+        p_count = len(players)
         x /= p_count
         y /= p_count
 
-        camera.animator.posToPosAndZoom(camera, (x, y), 0.5, 15)
+        camera.animator.posToPosAndZoom(camera, (x, y), 0.5, 10)
