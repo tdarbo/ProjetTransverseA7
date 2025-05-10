1468aeaa34c53c371ba747603c00012ff6bceb34
BastienP <bastien.pommard@efrei.net>
2025-05-11 01:08:54 +0200

    Remplacement héritage de Text par attribut self.text
    Signed-off-by: matte <matteo.spindler17@gmail.com>

--- code/broadcast.py
+++ code/broadcast.py
@@ -1,13 +1,24 @@
+import pygame
+
 from ui_text import Text
 from settings import *
 
 
-class BroadcastManager(Text):
+class BroadcastManager:
     def __init__(self):
         """Gère les messages à l'écran."""
         # On utilise Text dans ui_text pour gérer l'affichage du broadcast_manager
-        super().__init__(text="", pos=(WINDOW_WIDTH // 2, 20), font_size=15, color = (255,255,255) ,font_name=FONT_PATH, align="center")
-        self.duration = 5
+        self.text = Text(
+            text="",
+            pos=(
+                WINDOW_WIDTH // 2,
+                OVERLAY_MENU_MARGIN + OVERLAY_MENU_PADDING + 6
+            ),
+            font_size=12,
+            color=(255, 255, 255),
+            align="center"
+        )
+        self.duration = 15
         self.begintimer = 0
         self.timeout = True
 
@@ -16,12 +27,16 @@ class BroadcastManager(Text):
         if not self.timeout:
             # On draw le texte de l'objet Text (dans ui_text)
             # gestion du fond
-            padding = 6  # arbitraire
-            background_rect = self.rect.inflate(padding * 2 , padding * 2 )
-            pygame.draw.rect(screen, (213, 85, 52), background_rect)
-            pygame.draw.rect(screen, '#3F170D', background_rect, width=1)
-            super().draw(screen)
-            # vérifie le timer
+            bg_rect = pygame.Rect(
+                self.text.rect.x - OVERLAY_MENU_PADDING,
+                self.text.rect.y - OVERLAY_MENU_PADDING,
+                self.text.width + 2 * OVERLAY_MENU_PADDING,
+                self.text.height + 2 * OVERLAY_MENU_PADDING - 6
+            )
+            pygame.draw.rect(screen, (213, 85, 52), bg_rect)
+            pygame.draw.rect(screen, '#3F170D', bg_rect, width=3)
+            self.text.draw(screen)
+            # Vérifie le timer
             self.timer()
 
     def broadcast(self, message):
@@ -29,10 +44,9 @@ class BroadcastManager(Text):
         # Comme BroadcastManager est un enfant de Text,
         # on peut accéder aux méthodes et aux propriétés de Text via le self de BroadcastManager
         # set_text est définie dans Text et permet de changer le texte affiché.
-        self.set_text(message)
-        self.begintimer = pygame.time.get_ticks()
+        self.text.set_text(message)
         self.timeout = False
-        self.set_position((WINDOW_WIDTH // 2, 20), align="midtop")
+        self.begintimer = pygame.time.get_ticks()
 
     def timer(self):
         """Vérifie si le temps d'affichage est écoulé."""
