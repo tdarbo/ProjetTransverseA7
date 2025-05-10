5050399b1b1a96c4d990f37cb00da2304592b193
matte <matteo.spindler17@gmail.com>
2025-05-10 17:25:27 +0200

    Ajout écran avant chaque niveau et à la fin d'une partie
    Signed-off-by: matte <matteo.spindler17@gmail.com>

--- code/scene_play.py
+++ code/scene_play.py
@@ -6,26 +6,40 @@ from score import ScoreManager
 from map import Map
 from scene_manager import Scene
 from broadcast import BroadcastManager
+from ui_text import Text
 
 class PlayScene(Scene):
     """Gestion de la scène principale du jeu."""
 
+    # États possibles de la partie
+    STATE_LEVEL_INFO = "level_info"  # écran d'info du niveau
+    STATE_PLAYING = "playing"  # jeu en cours
+    STATE_END_INFO = "end_info"  # écran de fin de partie
+    DURATION_END_INFO = 5
+    DURATION_LEVEL_INFO = 3
+
     def __init__(self, height_index, game):
         super().__init__(height_index, game)
         self.game = game
-        self.players = []
+        self.players: list[Player] = []
         self.selected_maps = []
-        self.current_level = None
-        self.levels_played = 0
+        self.cur_level: Level = None
+        self.cur_level_index: int = 0
 
-        self.score_manager = None
-        self.broadcast_manager = BroadcastManager()
+        self.score_manager: ScoreManager = None
+        self.broadcast_manager: BroadcastManager = BroadcastManager()
 
-    def select_maps(self, count):
-        """Sélectionne 'count' maps uniques aléatoirement."""
-        maps = list(self.game.maps.values())
-        selected_maps = random.sample(maps, count)
-        return selected_maps
+        # Attributs pour la gestion des états
+        self.current_state: str = self.STATE_LEVEL_INFO
+        self.state_timer: float = 0  # Compteur en millisecondes
+        self.state_duration: int = 3000  # Durée en millisecondes (3 secondes)
+
+        # Surfaces pour les écrans d'information
+        self.level_info_surface: pygame.Surface = pygame.Surface(self.game.screen.get_size())
+        self.end_info_surface: pygame.Surface = pygame.Surface(self.game.screen.get_size())
+
+        # Nouvel attribut pour indiquer si la partie est terminée
+        self.finished: bool = False
 
     def on_enter(self):
         game_info = self.game.game_info
@@ -33,11 +47,17 @@ class PlayScene(Scene):
         holes_number = game_info.get("holes", 1)
 
         self.score_manager = ScoreManager(self.players, holes_number)
-        self.selected_maps = self.select_maps(holes_number)
-        self.levels_played = 0
+        self.select_maps(holes_number)
+        self.cur_level_index = -1
+        self.finished = False
 
         self.load_level()
         self.game.sound_manager.play_music(MUSICS["game1"])
+
+    def select_maps(self, count):
+        """Sélectionne 'count' maps uniques aléatoirement."""
+        maps = list(self.game.maps.values())
+        self.selected_maps = random.sample(maps, count)
 
     def create_players(self, names):
         """Instancie les joueurs avec leur couleur et nom."""
@@ -68,22 +88,31 @@ class PlayScene(Scene):
 
     def load_level(self):
         """Charge le niveau suivant ou termine la partie."""
-        if self.levels_played < len(self.selected_maps):
+        self.cur_level_index += 1
+
+        if self.cur_level_index < len(self.selected_maps):
             self.reset_players()
-            map_info = self.selected_maps[self.levels_played]
-            self.current_level = self.create_level(self.levels_played, map_info)
-            self.score_manager.set_current_hole(self.levels_played)
-            print(f"Niveau {self.levels_played + 1} chargé.")
-            self.levels_played += 1
+            map_info = self.selected_maps[self.cur_level_index]
+
+            self.cur_level = self.create_level(self.cur_level_index, map_info)
+            self.score_manager.set_current_hole(self.cur_level_index)
+
+            # Configurer l'état pour afficher les infos du niveau
+            self.build_level_info_surface()
+            self.current_state = self.STATE_LEVEL_INFO
+            self.state_timer = pygame.time.get_ticks()
         else:
-            self.current_level = None
+            # Marquer la partie comme terminée
+            self.finished = True
+            self.cur_level = None
+            self.build_end_info_surface()
+            self.current_state = self.STATE_END_INFO
+            self.state_timer = pygame.time.get_ticks()
 
     def next_level(self):
         """Passe au niveau suivant ou termine la partie."""
-        if self.levels_played < len(self.selected_maps):
+        if not self.finished:
             self.load_level()
-        else:
-            self.quit_game()
 
     def reset_players(self):
         for player in self.players:
@@ -91,140 +120,108 @@ class PlayScene(Scene):
             player.reset()
 
     def process_event(self, event):
-        if self.current_level:
-            self.current_level.process_event(event)
+        if (self.current_state == self.STATE_PLAYING) and not self.finished:
+            self.cur_level.process_event(event)
 
     def update(self, dt):
-        if self.current_level:
-            self.current_level.update(dt)
-            if self.current_level.finished:
-                self.next_level()
+        current_time = pygame.time.get_ticks()
+
+        # Gérer les transitions d'état
+        if self.current_state == self.STATE_LEVEL_INFO:
+            if current_time - self.state_timer >= self.state_duration:
+                self.current_state = self.STATE_PLAYING
+        elif self.current_state == self.STATE_PLAYING:
+            if self.cur_level:
+                self.cur_level.update(dt)
+                if self.cur_level.finished:
+                    self.next_level()
+        elif self.current_state == self.STATE_END_INFO:
+            if current_time - self.state_timer >= self.state_duration:
+                self.leave_game()
 
     def draw(self, screen):
         screen.fill("#BDDFFF")
-        if self.current_level:
-            self.current_level.draw(screen)
+        if self.current_state == self.STATE_LEVEL_INFO:
+            # On affiche l'écran d'info du niveau
+            screen.blit(self.level_info_surface, (0, 0))
+        elif self.current_state == self.STATE_PLAYING:
+            # On affiche le jeu
+            if self.cur_level:
+                self.cur_level.draw(screen)
+        elif self.current_state == self.STATE_END_INFO:
+            # On affiche l'écran de fin
+            screen.blit(self.end_info_surface, (0, 0))
 
-    def quit_game(self):
+    def leave_game(self):
         print("Partie terminée ! Scores finaux :")
+        self.game.scene_manager.change("start_menu_scene")
 
-# import random
-# from settings import *
-# from player import Player
-# from level import Level
-# from score import ScoreManager
-# from map import Map
-# from scene_manager import Scene
-# from broadcast import BroadcastManager
-#
-#
-# class PlayScene(Scene):
-#     """Gestion de la scène principale du jeu"""
-#
-#     def __init__(self, height_index, game):
-#         super().__init__(height_index, game)
-#         self.maps = game.maps
-#         self.screen = game.screen
-#
-#         self.game_info = None
-#
-#         # Création des joueurs et init score
-#         self.players = []
-#         self.score_manager = None
-#         self.broadcast_manager = None
-#
-#         # Gestion des niveaux joués
-#         self.hole_number = 0
-#         self.levels_played = 0
-#         self.current_level = None
-#
-#
-#     def on_enter(self):
-#         self.game_info = self.game.game_info
-#         self.players = self.create_players()
-#         if self.game_info["holes"] :
-#             self.hole_number = self.game_info["holes"]
-#         else :
-#             self.hole_number = 1
-#         self.score_manager = ScoreManager(self.players, self.hole_number)
-#         self.broadcast_manager = BroadcastManager()
-#         self.levels_played = 0
-#         self.hole_number = self.hole_number
-#         self.current_level = None
-#         # Chargement du premier niveau
-#         self.load_next_level()
-#         self.game.sound_manager.play_music(MUSICS["game1"])
-#
-#     def create_players(self):
-#         """Instancie les joueurs en utilisant les noms fournis."""
-#         players = []
-#         for i, name in enumerate(self.game_info.get("names", [])):
-#             player = Player(
-#                 color=PLAYER_COLORS[i % len(PLAYER_COLORS)],
-#                 position=Vector(0, 0),
-#                 radius=BALL_RADIUS,
-#                 mass=BALL_MASS,
-#                 name=name
-#             )
-#             players.append(player)
-#
-#         return players
-#
-#     def create_level(self, hole_number, map_infos):
-#         """Crée un niveau à partir du path de la map."""
-#         tiled_map = Map(map_infos, self.screen)
-#         width, height = self.screen.get_size()
-#         level = Level(
-#             hole_number,
-#             tiled_map,
-#             self.players,
-#             self.score_manager,
-#             self.broadcast_manager,
-#             self.game,
-#         )
-#         return level
-#
-#     def load_next_level(self):
-#         """Charge un niveau s'il en reste, sinon termine la partie."""
-#         if self.levels_played < self.hole_number:
-#             self.reset_players()
-#             map_key = random.choice(list(self.maps.keys()))
-#             map_infos = self.maps[map_key]
-#             self.current_level = self.create_level(self.levels_played, map_infos)
-#             self.levels_played += 1
-#             self.score_manager.set_current_hole(self.levels_played - 1)
-#             print("Current hole : ", self.levels_played - 1)
-#             print(f"Niveau {self.levels_played} chargé avec la map '{map_key}'.")
-#         else:
-#             self.current_level = None
-#
-#     def next_level(self):
-#         """Passe au niveau suivant ou quitte le jeu."""
-#         if self.levels_played < self.hole_number:
-#             self.load_next_level()
-#         else:
-#             self.quit_game()
-#
-#     def reset_players(self):
-#         for player in self.players:
-#             player.hide = False
-#             player.reset()
-#
-#     def process_event(self, event):
-#         if self.current_level:
-#             self.current_level.process_event(event)
-#
-#     def update(self, dt):
-#         if self.current_level:
-#             self.current_level.update(dt)
-#             if self.current_level.finished:
-#                 self.next_level()
-#
-#     def draw(self, screen):
-#         screen.fill("#BDDFFF")
-#         if self.current_level:
-#             self.current_level.draw(screen)
-#
-#     def quit_game(self):
-#         """Affiche les scores finaux et ferme le jeu."""
-#         print("Partie terminée ! Scores finaux :")
+    def build_level_info_surface(self):
+        """Construit la surface pour l'écran d'information de niveau."""
+        self.level_info_surface.blit(SPLASH_BG, (0, 0))
+
+        Text(
+            text=f"Trou n°{self.cur_level_index + 1}",
+            pos=(
+                self.game.screen.get_width() // 2,
+                self.game.screen.get_height() // 2 - 65
+            ),
+            font_size=17,
+            color="black",
+            align="center"
+        ).draw(self.level_info_surface)
+
+        Text(
+            text=self.cur_level.map.infos["name"],
+            pos=(
+                self.game.screen.get_width() // 2,
+                self.game.screen.get_height() // 2
+            ),
+            font_size=40,
+            color="#D55534",
+            align="center"
+        ).draw(self.level_info_surface)
+
+        Text(
+            text=f"Par {self.cur_level.map.infos["par"]}",
+            pos=(
+                self.game.screen.get_width() // 2,
+                self.game.screen.get_height() // 2 + 65
+            ),
+            font_size=16,
+            color="black",
+            align="center"
+        ).draw(self.level_info_surface)
+
+    def build_end_info_surface(self):
+        """Construit la surface pour l'écran de fin de partie."""
+        self.end_info_surface.blit(SPLASH_BG, (0, 0))
+
+        Text(
+            text="Partie terminée !",
+            pos=(
+                self.game.screen.get_width() // 2,
+                self.game.screen.get_height() // 2 - 115
+            ),
+            font_size=40,
+            color="#D55534",
+            align="center"
+        ).draw(self.end_info_surface)
+
+        Text(
+            text="Tableau récapitulatif des scores",
+            pos=(
+                self.game.screen.get_width() // 2,
+                self.game.screen.get_height() // 2 - 70
+            ),
+            font_size=16,
+            color="black",
+            align="center"
+        ).draw(self.end_info_surface)
+
+        self.score_manager.draw_menu(
+            start_x=self.game.screen.get_width() // 2 - self.score_manager.menu_width // 2 + 20,
+            start_y=self.game.screen.get_height() // 2 - self.score_manager.menu_height // 2 + 70,
+            text_color="black",
+            screen=self.end_info_surface
+        )
